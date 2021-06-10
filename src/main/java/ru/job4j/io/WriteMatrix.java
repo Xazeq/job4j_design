6758e2769e14a6ae119ada1fca1ffb8b9858c273
Xazeq <setezey@gmail.com>
2021-06-10 16:40:14 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/WriteMatrix.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class WriteMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = Matrix.multiple(5);
+        writeTable(matrix);
+    }
+
+    public static void writeTable(int[][] matrix) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int[] array : matrix) {
+                for (int value : array) {
+                    String str = value + " ";
+                    out.write(str.getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
