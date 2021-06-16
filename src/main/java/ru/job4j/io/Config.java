7e9d0d3bfe4433344cb666f05b8bb7c327906b7e
Xazeq <setezey@gmail.com>
2021-06-16 09:12:27 +0300

    1. Читаем файл конфигурации [#858 #168015]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -5,10 +5,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
@@ -17,19 +15,17 @@ public class Config {
     public Config(String path) {
         this.path = path;
     }
-
     public void load() {
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            List<String> lines = read.lines()
+            read.lines()
                     .filter(line -> !line.startsWith("#") && !line.isEmpty())
-                    .collect(Collectors.toList());
-            for (var line : lines) {
-                String[] array = line.split("=");
-                if (array.length != 2) {
-                    throw new IllegalArgumentException();
-                }
-                values.put(array[0], array[1]);
-            }
+                    .forEach(line -> {
+                        String[] array = line.split("=");
+                        if (array.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(array[0], array[1]);
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
