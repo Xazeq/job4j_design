9d38fa6cbd88787e107cb2d034b2dc66586b0d5a
Xazeq <setezey@gmail.com>
2021-06-17 09:36:21 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,64 +1,81 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
+import java.io.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenFirstUnavailable() {
-        String source = "./data/first_unavailable_server.log";
-        String target = "./data/target.csv";
-        Analizy analizy = new Analizy();
-        analizy.unavailable(source, target);
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            StringBuilder result = new StringBuilder();
-            in.lines()
-                    .forEach(result::append);
-            String expected = "10:13:02;10:14:55;"
-                    + "10:15:12;10:15:22;";
-            assertEquals(result.toString(), expected);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenFirstUnavailable() throws IOException {
+        File source = folder.newFile("first_unavailable_server.log");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("400 10:13:02");
+            out.println("500 10:14:22");
+            out.println("300 10:14:55");
+            out.println("200 10:15:01");
+            out.println("500 10:15:12");
+            out.println("200 10:15:22");
         }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        String expected = "10:13:02;10:14:55;"
+                + "10:15:12;10:15:22;";
+        assertThat(result.toString(), is(expected));
     }
 
     @Test
-    public void whenMiddleUnavailable() {
-        String source = "./data/middle_unavailable_server.log";
-        String target = "./data/target.csv";
-        Analizy analizy = new Analizy();
-        analizy.unavailable(source, target);
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            StringBuilder result = new StringBuilder();
-            in.lines()
-                    .forEach(result::append);
-            String expected = "10:57:01;10:59:01;"
-                    + "11:01:02;11:02:02;";
-            assertEquals(result.toString(), expected);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenMiddleUnavailable() throws IOException {
+        File source = folder.newFile("first_unavailable_server.log");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        String expected = "10:57:01;10:59:01;"
+                + "11:01:02;11:02:02;";
+        assertThat(result.toString(), is(expected));
     }
 
     @Test
-    public void whenLastUnavailable() {
-        String source = "./data/last_unavailable_server.log";
-        String target = "./data/target.csv";
-        Analizy analizy = new Analizy();
-        analizy.unavailable(source, target);
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            StringBuilder result = new StringBuilder();
-            in.lines()
-                    .forEach(result::append);
-            String expected = "10:14:58;10:15:01;";
-            assertEquals(result.toString(), expected);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenLastUnavailable() throws IOException {
+        File source = folder.newFile("first_unavailable_server.log");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("300 10:14:55");
+            out.println("400 10:14:58");
+            out.println("200 10:15:01");
+            out.println("400 10:13:02");
+            out.println("500 10:14:22");
         }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        String expected = "10:14:58;10:15:01;";
+        assertThat(result.toString(), is(expected));
     }
 }
