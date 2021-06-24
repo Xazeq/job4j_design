95056489714490ce206974dc34a1584d746b1bca
Xazeq <setezey@gmail.com>
2021-06-24 17:19:21 +0300

    JSON, XML

---
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -0,0 +1,86 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Person {
+    private final boolean sex;
+    private final int age;
+    private final Contact contact;
+    private final String[] statuses;
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getStatuses() {
+        return statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(person));
+
+        final String personJson =
+                "{"
+                    + "\"sex\":false,"
+                    + "\"age\":35,"
+                    + "\"contact\":"
+                        + "{"
+                            + "\"phone\":\"+7(924)111-111-11-11\""
+                        + "},"
+                    + "\"statuses\":"
+                        + "[\"Student\", \"Free\"]"
+                + "}";
+        final Person personMod = gson.fromJson(personJson, Person.class);
+        System.out.println(personMod);
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        System.out.println(jsonObject);
+
+        System.out.println(new JSONObject(person));
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -0,0 +1,88 @@
+package ru.job4j.serialization.xml;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person {
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
+    private String[] status;
+
+    public Person() { }
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.status = statuses;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(status)
+                + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException {
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String result = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(result)) {
+            Person personFromXML = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(personFromXML);
+        }
+    }
+}
