cbc4cf447edfe6aacfcc67a845e3f0f014dfeb88
Xazeq <setezey@gmail.com>
2021-07-20 10:31:49 +0300

    Код с уроков

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -5,11 +5,15 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
-        Config config = new Config("./src/main/resources/app.properties");
+        ClassLoader loader = ConnectionDemo.class.getClassLoader();
+        String path = Objects.requireNonNull(loader.getResource("app.properties")).getPath();
+        Config config = new Config(path);
+        //Config config = new Config("./src/main/resources/app.properties");
         config.load();
         try (Connection connection = DriverManager.getConnection(
                 config.value("url"),
