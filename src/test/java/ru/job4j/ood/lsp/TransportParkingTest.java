4801ea667669e9ed714a4bf8557f2a2617f5f04f
Xazeq <setezey@gmail.com>
2021-09-01 10:46:32 +0300

    2. Парковка машин [#853 #197107] тесты исправления

--- src/test/java/ru/job4j/ood/lsp/TransportParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/TransportParkingTest.java
@@ -4,46 +4,47 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.is;
 
 public class TransportParkingTest {
 
     /*@Ignore
     @Test
     public void whenAddCarToParking() {
-        TransportParking parking = new TransportParking(4);
+        Parking parking = new TransportParking(4, 3);
         Transport car = new Car();
-        parking.park(car);
-        assertThat(parking.getNumberOccupiedPlace(), is(1));
-        assertThat(parking.getNumberAvailablePlace(), is(3));
+        assertTrue(parking.park(car));
     }
 
     @Ignore
     @Test
     public void whenAddTruckToParking() {
-        TransportParking parking = new TransportParking(4);
+        Parking parking = new TransportParking(2, 3);
         Transport truck = new Truck(3);
-        parking.park(truck);
-        assertThat(parking.getNumberOccupiedPlace(), is(3));
-        assertThat(parking.getNumberAvailablePlace(), is(1));
+        assertTrue(parking.park(truck));
     }
 
     @Ignore
     @Test
     public void whenAddCarAndTruckToParking() {
-        TransportParking parking = new TransportParking(4);
+        Parking parking = new TransportParking(2, 4);
         Transport truck = new Truck(3);
         Transport car = new Car();
-        parking.park(truck);
-        parking.park(car);
-        assertThat(parking.getNumberOccupiedPlace(), is(4));
-        assertThat(parking.getNumberAvailablePlace(), is(0));
+        assertTrue(parking.park(truck));
+        assertTrue(parking.park(car));
+    }
+
+    @Ignore
+    @Test
+    public void whenTruckOccupyCarPlace() {
+        Parking parking = new TransportParking(4, 0);
+        Transport truck = new Truck(3);
+        assertTrue(parking.park(truck));
     }
 
     @Ignore
     @Test
     public void whenNoPlaceToParkCar() {
-        TransportParking parking = new TransportParking(1);
+        Parking parking = new TransportParking(1, 4);
         Transport car = new Car();
         Transport car2 = new Car();
         parking.park(car);
@@ -53,7 +54,7 @@ public class TransportParkingTest {
     @Ignore
     @Test
     public void whenNoPlaceToParkTruck() {
-        TransportParking parking = new TransportParking(2);
+        Parking parking = new TransportParking(2, 0);
         Transport truck = new Truck(4);
         assertFalse(parking.park(truck));
     }*/
